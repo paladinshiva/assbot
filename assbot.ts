62eea2895828233775e446038d9ae126d133f41e
paladinshiva <paladinshiva@gmail.com>
2017-10-17 19:58:06 -0500

    TS

--- assbot.ts
+++ assbot.ts
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  console.log("I am ready!");
+  console.log("I am ready to kill!");
 });
 client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
