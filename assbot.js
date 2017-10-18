f95c52c74eb30cd0b4c0f66ccb7e50b962225c3d
paladinshiva <paladinshiva@gmail.com>
2017-10-17 19:50:51 -0500

    2

---
+++ assbot.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.login("MzY5OTYyODAxNjA0NTkxNjE2.DMgr0Q.Un1_77ZSJP6PCBik6NtOdbef_uA");
