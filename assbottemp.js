fb763886d7ed6fb5b7fcb11230bc126ea328bb8e
paladinshiva <paladinshiva@gmail.com>
2017-10-20 14:34:41 -0500

    Purge

--- assbottemp.js
+++ assbottemp.js
@@ -34,7 +34,6 @@ client.login(config.token);
 client.on("ready", () => {
   console.log("I am ready!");
 });
-
 client.on("message", message => {
 //Exiting if another bot messaged or prefix is incorrect
     if((message.author.bot) || (message.content.indexOf(config.prefix) !== 0)) return;
@@ -43,15 +42,13 @@ client.on("message", message => {
 //Attaching in-chat commands to the variable "command"
     var command = args.shift().toLowerCase();
     if (command === "purge"){
-      com_purge(args);
+        purge(args);
     }
 });
-
-function com_purge(args) {
-  var amount = parseInt(args[0], 10);
-  var fetched = message.channel.fetchMessages({count: amount});
-  message.channel.bulkDelete(fetched);
-  // message.channel.bulkDelete(20);
+function purge(args) {
+    var amount = parseInt(args[0], 10);
+    var fetched = message.channel.fetchMessages({count: amount});
+    message.channel.bulkDelete(fetched);
 }
 
 //Basic messages delete command
